e997821e53383fb3e09603f5f386a91c5cdb9074
Peter Nguyen <truonganh20062001@gmail.com>
2024-02-25 22:10:08 +0700

    introduction angular - typescript - intitial Backend project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# angular-and-spring-boot-e-commerce
+# Angular and Java Spring Boot E-Commerce Website
+
+Welcome to the Angular and Java Spring Boot E-Commerce Website Learning Repository! Here, you will find resources in Angular and Java Spring Boot. [Angular and Java Spring Boot E-Commerce Website Note](https://1drv.ms/x/s!AqwUXfqIrNg7shNjG87bYq0hyNNL?e=pkgRyU)
+
+## ⚙️ What you will find in this repo:
+- Spring Boot back-end using REST APIs and MySQL database
+- Process credit card payments using Stripe API
+- Angular front-end applications
+- Security: JWT, OAuth2, OpenID Connect and SSL/TLS
---
+++ angular-training/03-angular-conditional-formatting/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { SalesPerson } from './sales-person';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list-bootstrap.component.html',
+  styleUrl: './sales-person-list.component.css'
+})
+export class SalesPersonListComponent {
+  // array objects data mock
+  salesPersonList: SalesPerson[] =  [ 
+    new SalesPerson("test1", "testLastName1", "email1", 1000),
+    new SalesPerson("test2", "testLastName2", "email2", 2000),
+    new SalesPerson("test3", "testLastName3", "email3", 8000),
+    new SalesPerson("test4", "testLastName4", "email4", 7000),
+  ];
+
+}
